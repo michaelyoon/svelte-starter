4e01c7cd7c0269b41ee51f6470bd71dd6636a21b
Michael Yoon <michael@yoon.org>
2025-02-06 22:55:42 -0700

    Componentize dark mode switch and menu

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-	import { toggleMode } from "mode-watcher";
-	import Sun from "lucide-svelte/icons/sun";
-	import Moon from "lucide-svelte/icons/moon";
-	import { Button } from "$lib/components/ui/button/index.js";
+	import DarkModeSwitch from "./dark-mode-switch.svelte";
+	import DarkModeMenu from "./dark-mode-menu.svelte";
 	import { PUBLIC_APP_NAME } from "$env/static/public";
 
     let { user } = $props();
@@ -16,15 +14,9 @@
     </h1>
 
     <div class="flex items-center gap-2">
-        <Button onclick={toggleMode} variant="outline" size="icon">
-            <Sun
-                class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-            />
-            <Moon
-                class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-            />
-            <span class="sr-only">Toggle theme</span>
-        </Button>
+        <!-- Choose one of these two components: -->
+        <DarkModeSwitch />
+        <DarkModeMenu />
 
         {#if user}
             <form action="/logout" method="post">
